6c0843bcd1088969073756790c21d473d32b109b
Bearly-Codes <nickabogan@gmail.com>
2023-07-31 10:40:48 -0400

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,37 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import StarBackground from "./StarBackground";
-import React, {useEffect, useState} from 'react';
-import AboutMe from "./AboutMe";
-import Projects from "./Projects";
-import Footer from "./Footer";
 
-//TODO Add a navbar
-
-//TODO Add education section + mention TA experience + Certifications
-const App = () => {
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, []);
-
-    //Lights up when user clicks the "get in touch" link
-    const handleLinkClick = () => {
-        window.scrollTo(0, document.body.scrollHeight);
-        setFooterLight(true);
-        setTimeout(() => setFooterLight(false), 3000);
-    };
-
-    const [footerLight, setFooterLight] = useState(false);
-
-    return (
-        <div>
-            <div className="star-background">
-                <StarBackground />
-            </div>
-            <AboutMe onLinkClick={handleLinkClick} />
-            <Projects />
-            <Footer id="footer" isLit={footerLight}/>
-        </div>
-    );
-};
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;

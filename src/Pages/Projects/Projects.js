d54cb4964b188df861002b9992a3eb0e2e25fc8b
BuildTools <unconfigured@null.spigotmc.org>
2024-05-13 21:07:39 -0400

    added media breakpoints for small screens and mobile

--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -16,7 +16,7 @@ function Projects() {
                 What I've Done
             </Row>
             <Row className="Projects-content">
-                <Col className="Projects-box">
+                <Col md={6} sm={12} className="Projects-box">
                     <a href="https://github.com/Bearly-Codes/Bearly-Codes.github.io" target="_blank" style={{ textDecoration: 'none', display: 'inline-block' }}>
                         <img src={Nyx} alt="Nyx" />
                     </a>
@@ -32,8 +32,8 @@ function Projects() {
                         </svg>
                     </Row>
                 </Col>
-                <Col className="Projects-box">
-                    <a href="https://github.com/jackpeng01/BoilerBytes" target="_blank" style={{ textDecoration: 'none', display: 'inline-block' }}>
+                <Col md={6} sm={12} className="Projects-box">
+                    <a href="https://github.com/jackpeng01/BoilerB</Col>ytes" target="_blank" style={{ textDecoration: 'none', display: 'inline-block' }}>
                         <img src={BoilerBytes} alt="Boiler Bytes" />
                     </a>
                     <Row className="Projects-proj-title">
@@ -47,13 +47,13 @@ function Projects() {
                     <Row className="Projects-icon">
 
                         <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="bi bi-github" viewBox="0 0 16 16">
-                            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
+                            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54</svg> 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
                         </svg>
                     </Row>
                 </Col>
             </Row>
             <Row className="Projects-content">
-                <Col className="Projects-box">
+                <Col md={6} sm={12} className="Projects-box">
                     <img src={ASMProfiler} alt="ASMProfiler" />
 
                     <Row className="Projects-proj-title">
@@ -65,7 +65,7 @@ function Projects() {
                         to hundreds of students for use in the Purdue Computer Architecture course.
                     </Row>
                 </Col>
-                <Col className="Projects-box">
+                <Col md={6} sm={12} className="Projects-box">
                     <img src={Bunker} alt="Bunker" />
 
                     <Row className="Projects-proj-title">

51e2c380c9febde3655405ba8838b3f724162ef5
BuildTools <unconfigured@null.spigotmc.org>
2025-08-29 17:26:55 -0400

    updated for my senior year

--- src/Pages/AboutMe/Aboutme.js
+++ src/Pages/AboutMe/Aboutme.js
@@ -13,7 +13,7 @@ function Aboutme() {
                     Who I Am
                 </Row>
                 <Row className="Aboutme-content">
-                I'm Nick Bogan, a Sophomore Computer Science student at Purdue, and I build things. Always have. My childhood was filled with K'Nex, Legos, Train sets, and Tinker Toys. When I got my hands on a computer I immediately got to work building things there too. I was making games on Scratch before I'd left elementary school, in middle school I was coding robots, in highschool websites, and in college apps, tools, toys, and, well, more websites. When I needed a break I'd play video games and build spaceships, factories, cities, and submarines. I build things, and if you need something built chances are I can help.  Get in touch, let's build something together.
+                I'm Nick Bogan, a senior in Computer Science at Purdue, and I build things. Always have. My childhood was filled with K'Nex, Legos, Train sets, and Tinker Toys. When I got my hands on a computer I immediately got to work building things there too. I was making games on Scratch before I'd left elementary school, in middle school I was coding robots, in highschool websites, and in college apps, tools, toys, and, well, more websites. When I needed a break I'd play video games and build spaceships, factories, cities, and submarines. I build things, and if you need something built chances are I can help.  Get in touch, let's build something together.
                 </Row>
             </Container>        
     );
--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -5,7 +5,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import BoilerBytes from '../../Images/BoilerBytes.jpg';
 import ASMProfiler from '../../Images/ASMProfiler.png';
-import Bunker from '../../Images/Bunker.png';
+import SunriseOS from '../../Images/SunriseOS.png';
 import Nyx from '../../Images/Nyx.png';
 
 function Projects() {
@@ -17,14 +17,15 @@ function Projects() {
             </Row>
             <Row className="Projects-content">
                 <Col md={6} sm={12} className="Projects-box">
-                    <a href="https://github.com/Bearly-Codes/Bearly-Codes.github.io" target="_blank" style={{ textDecoration: 'none', display: 'inline-block' }}>
-                        <img src={Nyx} alt="Nyx" />
+                    <a href="https://github.com/Bearly-Codes/SunriseOS" target="_blank" style={{ textDecoration: 'none', display: 'inline-block' }}>
+                        <img src={SunriseOS} alt="Sunrise OS" />
                     </a>
+
                     <Row className="Projects-proj-title">
-                        Nyx
+                        Sunrise OS
                     </Row>
                     <Row className="Projects-proj-content">
-                        My personal website built with react and bootstrap, styled with SASS.
+                        An OS built with the help of OSDev.org resources, currently has functional interrupts and printing capabilities. In progress.
                     </Row>
                     <Row className="Projects-icon">
                         <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="bi bi-github" viewBox="0 0 16 16">
@@ -65,13 +66,19 @@ function Projects() {
                     </Row>
                 </Col>
                 <Col md={6} sm={12} className="Projects-box">
-                    <img src={Bunker} alt="Bunker" />
-
+                    <a href="https://github.com/Bearly-Codes/Bearly-Codes.github.io" target="_blank" style={{ textDecoration: 'none', display: 'inline-block' }}>
+                        <img src={Nyx} alt="Nyx" />
+                    </a>
                     <Row className="Projects-proj-title">
-                        Bunker
+                        Nyx
                     </Row>
                     <Row className="Projects-proj-content">
-                        A prototype of an idle game that was programmed with ReactJS, currently being rebuilt in typescript.
+                        My personal website built with react and bootstrap, styled with SASS.
+                    </Row>
+                    <Row className="Projects-icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="bi bi-github" viewBox="0 0 16 16">
+                            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
+                        </svg>
                     </Row>
                 </Col>
             </Row>
